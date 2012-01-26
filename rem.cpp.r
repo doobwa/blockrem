1645ba2cc984f0b34b8f30bedff644f0d0b5f264
Chris DuBois <chris.dubois@gmail.com>
2012-01-25 22:38:00 -0800

    Fix 3d array indexing.  Implement log rate array.

--- rem.cpp.r
+++ rem.cpp.r
@@ -4,8 +4,14 @@ library(Rcpp)
 fx <- cxxfunction(,"",includes=
   '
 int threeDIndex(int j, int k, int l, int J, int K, int L) { 
-  return j*K*L + k*L + l;
+  return l*J*K + k*J + j;
 }
+Rcpp::IntegerVector testThreeDIndex() {
+  Rcpp::IntegerMatrix mp = Rcpp::IntegerMatrix(Dimension(3,4,5));
+  mp[threeDIndex(1,1,1,3,4,5)] = 1;
+  return mp;
+}
+
 int testIntConv(Rcpp::IntegerVector ps) {
   int r;
   if (ps[0] == 1) {
@@ -16,7 +22,6 @@ int testIntConv(Rcpp::IntegerVector ps) {
   int k = ps[ps[0]];
   return k;
 }
-
 double computeLambdaOld(int i, int j, int a, int b, Rcpp::NumericVector beta) {
   double lam = 0;
     lam += beta[0];
@@ -69,7 +74,7 @@ double computeLambda(int i, int j, int a, int b, Rcpp::NumericVector beta,Rcpp::
 // Current "weirdness": Assumes all events "occur" at time 0.
 Rcpp::List llk(Rcpp::NumericVector beta, Rcpp::NumericVector times, Rcpp::IntegerVector sen, Rcpp::IntegerVector rec, Rcpp::IntegerVector ix, Rcpp::IntegerVector jx,Rcpp::IntegerVector px, int N, int M, int P){
   // last event id that lam_ij changed
-  Rcpp::IntegerMatrix mp = Rcpp::IntegerMatrix(N,N); //TODO +1 hack? 
+  Rcpp::IntegerMatrix mp = Rcpp::IntegerMatrix(N,N); 
 
   double llk = 0;
   int a,b,i,j,r;
@@ -83,17 +88,15 @@ Rcpp::List llk(Rcpp::NumericVector beta, Rcpp::NumericVector times, Rcpp::Intege
     // Loop through dyads (i,r) and (r,j) whose intensities change due to event m
     for (int v = 0; v < jx.size(); v++) {
       r = jx[v];
+      // Sender/receiver of last event involving i or r
       a = sen[mp(i,r)];
       b = rec[mp(i,r)];
       lam = computeLambda(i,r,a,b,beta,px);
-      //Rprintf("%f\\n",times[mp(i,r)]);
       llk -= (times[m] - times[mp(i,r)]) * exp(lam);
-      mp(i,r) = m;  // update mp
-      a = sen[mp(r,i)];
-      b = rec[mp(r,i)];
       lam = computeLambda(r,i,a,b,beta,px);
       llk -= (times[m] - times[mp(r,i)]) * exp(lam);
-      mp(r,i) = m;  // update mp
+      mp(i,r) = m;  // update mp
+      mp(r,i) = m;
     }
     for (int v = 0; v < ix.size(); v++) {
       r = ix[v];
@@ -101,54 +104,41 @@ Rcpp::List llk(Rcpp::NumericVector beta, Rcpp::NumericVector times, Rcpp::Intege
       b = rec[mp(j,r)];
       lam = computeLambda(j,r,a,b,beta,px);
       llk -= (times[m] - times[mp(j,r)]) * exp(lam);
-      mp(j,r) = m;  // update mp
-      a = sen[mp(r,j)];
-      b = rec[mp(r,j)];
       lam = computeLambda(r,j,a,b,beta,px);
       llk -= (times[m] - times[mp(r,j)]) * exp(lam);
-      mp(r,j) = m;  // update mp
+      mp(j,r) = m;  // update mp
+      mp(r,j) = m; 
     }
   }
   return Rcpp::List::create(Rcpp::Named( "llk" ) = llk);
 }
 
+// Compute (M,N,N) array of log rates, where the (m,i,j) element is log lambda_{i,j}(t_m) (and is therefore the value of that intensity function since the last time lambda_{i,j} changed).
 Rcpp::NumericVector lrm(Rcpp::NumericVector beta, Rcpp::NumericVector times, Rcpp::IntegerVector sen, Rcpp::IntegerVector rec, Rcpp::IntegerVector ix, Rcpp::IntegerVector jx,Rcpp::IntegerVector px, int N, int M, int P){
-  // last event id that lam_ij changed
-  Rcpp::NumericMatrix mp = Rcpp::NumericMatrix(N+1,N+1); //TODO +1 hack? 
+
+  Rcpp::NumericMatrix mp = Rcpp::NumericMatrix(N,N); 
   Rcpp::NumericVector lrm = Rcpp::NumericVector(Dimension(M,N,N));
 
-  int a,b,i,j,r;
-
+  int a,b;
   for (int m = 1; m<M; m++) {
-
-    i = sen[m];
-    j = rec[m];
-
-    // Loop through dyads (i,r) and (r,j) whose intensities change due to event m
-    for (int v = 0; v < jx.size(); v++) {
-      r = jx[v];
-      a = sen[mp(i,r)];
-      b = rec[mp(i,r)];
-      lrm[threeDIndex(m,i,r,M,N,N)] = computeLambda(i,r,a,b,beta,px);
-      mp(i,r) = m;  // update mp
-      a = sen[mp(r,i)];
-      b = rec[mp(r,i)];
-      lrm[threeDIndex(m,r,i,M,N,N)] = computeLambda(r,i,a,b,beta,px);
-      mp(r,i) = m;  // update mp
+    for (int i=0;i<N;i++) {
+      for (int j=0;j<N;j++) {
+        // get dyad that occurred last time either i or j was involved
+        a = sen[mp(i,j)];
+        b = rec[mp(i,j)];
+        // compute rate for lambda_ij(t_m)
+        lrm[threeDIndex(m,i,j,M,N,N)] = computeLambda(i,j,a,b,beta,px);
+      }
     }
-    for (int v = 0; v < ix.size(); v++) {
-      r = ix[v];
-      a = sen[mp(j,r)];
-      b = rec[mp(j,r)];
-      lrm[threeDIndex(m,j,r,M,N,N)] = computeLambda(j,r,a,b,beta,px);
-      mp(j,r) = m;  // update mp
-      a = sen[mp(r,j)];
-      b = rec[mp(r,j)];
-      lrm[threeDIndex(m,r,j,M,N,N)] = computeLambda(r,j,a,b,beta,px);
-      mp(r,j) = m;  // update mp
+    // save which lambdas changed because of (i,j) occurring at time m
+    for (int v = 0; v < N; v++) {
+      mp(sen[m],v) = m;
+      mp(v,sen[m]) = m;
+      mp(rec[m],v) = m;
+      mp(v,rec[m]) = m;
     }
   }
-  return lrm;//Rcpp::List::create(Rcpp::Named( "lrm" ) = lrm);
+  return lrm;
 }
 RCPP_MODULE(drem){
   function( "llk", &llk ) ;
@@ -156,6 +146,7 @@ RCPP_MODULE(drem){
   function( "testIntConv", &testIntConv );
   function( "computeLambda", &computeLambda);
   function( "computeLambdaOld", &computeLambdaOld ) ;
+  function( "testThreeDIndex", &testThreeDIndex ) ;
 }
 ', plugin="Rcpp")
 
--- test.rem.cpp.r
+++ test.rem.cpp.r
@@ -1,29 +1,33 @@
 source("rem.cpp.r")
 library(testthat)
+
+set.seed(1)
+M <- 100
+N <- 10
+P <- 6
+times <- sort(runif(M,0,100))
+sen <- sample(1:N,M,replace=TRUE) - 1
+rec <- sample(1:N,M,replace=TRUE) - 1
+beta <- rnorm(P)
+ix <- 0:(N-1)
+px <- rep(1,7)
+
 test_that("llk works on different subsets of dyads",{
   
-  set.seed(1)
-  M <- 5000
-  N <- 100
-  P <- 6
-  times <- sort(runif(M,0,100))
-  sen <- sample(1:N,M,replace=TRUE) - 1
-  rec <- sample(1:N,M,replace=TRUE) - 1
-  beta <- rnorm(P)
-  ix <- 0:(N-1)
-  px <- rep(1,7)
   llk <- drem$llk(beta,times,sen,rec,ix,ix,px,N,M,P)
   llk
   N <- 10
   ix <- 1:5 - 1
   jx <- 5:10 - 1
-  sen <- sample(ix,M,replace=TRUE) - 1
-  rec <- sample(jx,M,replace=TRUE) - 1
+  sen <- sample(ix,M,replace=TRUE)
+  rec <- sample(jx,M,replace=TRUE)
   llk <- drem$llk(beta,times,sen,rec,ix,jx,px,N,M,P)
 })
 
-test_that("log rate matrix computation works",{
-  lrm <- drem$lrm(beta,times,sen,rec,ix,jx,px,N,M,P)
+test_that("log rate matrix computation correct",{
+  lrm <- drem$lrm(beta,times,sen,rec,ix,ix,px,N,M,P)
+  expect_that(lrm[2,1,1],equals(beta[1]))
+  expect_that(lrm[2,rec[1]+1,sen[1]+1],equals(beta[1] + beta[2]))  # AB-BA
 })
 
 test_that("calculating lambda runs",{
