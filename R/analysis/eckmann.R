03543a145aefd76d9054378a4c275bec12cf4bc1
Chris DuBois <chris.dubois@gmail.com>
2012-01-31 10:12:45 -0800

    Split up process/analysis of eckmann data.

--- R/analysis/eckmann.R
+++ R/analysis/eckmann.R
@@ -1,26 +1,3 @@
-d <- read.table("eckmann/eckmann.txt",header=FALSE)
-colnames(d) <- c("from","to","size","timestamp","chron",
-                 "weekday","multiplicity")
-d <- subset(d,multiplicity==1)
-times <- d$chron - d$chron[1]
-d$ntime <- times/max(times)
-all <- sort(unique(c(d$from,d$to)))
-min.count <- 50
-max.count <- 1000
-counts <- table(c(d$from,d$to))
-ix <- which(counts > min.count & counts < max.count)
-length(ix)
-keep <- as.numeric(names(counts[ix]))
-ix <- which(d$from %in% keep & d$to %in% keep)
-d <- d[ix,c("ntime","from","to")]
-              plot(as.numeric(table(table(c(d$from,d$to)))),type="l")
-
-nodes <- sort(unique(c(d$from,d$to)))
-d$s <- as.numeric(factor(d$from,nodes))
-d$r <- as.numeric(factor(d$to,nodes))
-
-A <- d[,c("ntime","s","r")]
-
 
 
 # Visualize
@@ -32,30 +9,14 @@ plotspmat <- function(A) {
 }
 plotspmat(A)
 
-# Create even smaller subset
-ix <- which(A[,2] %in% 40:130 & A[,3] %in% 40:130)
-A <- A[ix,]
-A[,2] <- as.numeric(factor(A[,2],40:130))
-A[,3] <- as.numeric(factor(A[,3],40:130))
 
-# Reorder names
-B <- A
-all <- sort(unique(c(A[,2],A[,3])))
-all <- sample(all)
-A[,2] <- match(A[,2],all)
-A[,3] <- match(A[,3],all)
-plotspmat(A)
-
-source("rem.cpp.r")
-source("fns.r")
 N <- length(unique(c(A[,2],A[,3])))
 K <- 2
-P <- 7
 M <- nrow(A)
 
 z <- sample(1:K,N,replace=TRUE)
-beta <- array(rnorm(K^2*P),c(K,K,P))
-system.time(brem.llk(A,N,z,beta,px))
+beta <- array(rnorm(K^2*P),c(P,K,K))
+system.time(brem.llk(A,N,z,beta))
 
 set.seed(4)
 niter <- 100
---
+++ R/process/eckmann.r
@@ -0,0 +1,39 @@
+d <- read.table("data/eckmann/eckmann.txt",header=FALSE)
+colnames(d) <- c("from","to","size","timestamp","chron",
+                 "weekday","multiplicity")
+d <- subset(d,multiplicity==1)
+times <- d$chron - d$chron[1]
+d$ntime <- times/max(times)
+all <- sort(unique(c(d$from,d$to)))
+min.count <- 50
+max.count <- 1000
+counts <- table(c(d$from,d$to))
+ix <- which(counts > min.count & counts < max.count)
+length(ix)
+keep <- as.numeric(names(counts[ix]))
+ix <- which(d$from %in% keep & d$to %in% keep)
+d <- d[ix,c("ntime","from","to")]
+plot(as.numeric(table(table(c(d$from,d$to)))),type="l")
+
+nodes <- sort(unique(c(d$from,d$to)))
+d$s <- as.numeric(factor(d$from,nodes))
+d$r <- as.numeric(factor(d$to,nodes))
+
+A <- d[,c("ntime","s","r")]
+
+save(A,file="data/eckmann/dyadic.rdata")
+
+# Create even smaller subset
+ix <- which(A[,2] %in% 40:130 & A[,3] %in% 40:130)
+A <- A[ix,]
+A[,2] <- as.numeric(factor(A[,2],40:130))
+A[,3] <- as.numeric(factor(A[,3],40:130))
+
+# Reorder names
+B <- A
+all <- sort(unique(c(A[,2],A[,3])))
+all <- sample(all)
+A[,2] <- match(A[,2],all)
+A[,3] <- match(A[,3],all)
+
+save(A,file="data/eckmann/dyadic-small.rdata")
