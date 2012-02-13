775445a8199bf0bd83bed38779a7f2116cd36a4d
Chris DuBois <chris.dubois@gmail.com>
2012-02-13 09:29:25 -0800

    Fixing parallel calls in bash script.

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 parallel ./R/brem.Rscript -f "data/eckmann.dyadic.small.rdata" -k {1} -n 1000 -m {2} -d data/eckmann ::: 2 3 5 ::: "baserates" "single" "shared" "full" &
 seq 4 | parallel --sshlogin 8/d9,8/d10,8/d11 -j 3 'cd /extra/duboisc0/blockrem; ./R/brem.Rscript -f "data/eckmann.dyadic.small.rdata" -k 2 -n 1000 -m "shared" -d data/eckmann'
-parallel --sshlogin 8/d9,8/d10,8/d11 -j 4 'cd /extra/duboisc0/blockrem; ./R/brem.Rscript -f "data/eckmann.dyadic.small.rdata" -k {1} -n 1000 -m {2} -d data/eckmann ::: 2 3 5 ::: "baserates" "single" "shared" "full" &'
+seq 3 | parallel --sshlogin 8/d10,8/d11 './tmp.Rscript'
+ -f "data/eckmann.dyadic.small.rdata" -k {1} -n 1000 -m {2} -d data/eckmann ::: 2 3 5 ::: "baserates" "single" "shared" "full" &'
 
 ./R/brem.Rscript -f "data/eckmann.dyadic.small.rdata" -k 1 -n 2000 -m "full" -d "results/eckmann-small"
 
@@ -13,14 +14,24 @@ parallel --sshlogin 8/d9,8/d10,8/d11 -j 4 'cd /extra/duboisc0/blockrem; ./R/brem
 ./R/brem.Rscript -f "data/synthetic.rdata" -k 1 -n 2000 -m "full" -d "results/synthetic"
 
 # Use this now:
-./brem.r -f "data/synthetic.rdata" -k 2 -n 1000 -m {} -d "results/synthetic" -g "slow" ::: "full" "slow"
+./brem.r -f "data/synthetic.rdata" -k 2 -n 500 -m "baserates" -d "results/synthetic" -g "fast"
+./brem.r -f "data/synthetic.rdata" -k 2 -n 500 -m "shared" -d "results/synthetic" -g "fast"
+./brem.r -f "data/synthetic.rdata" -k 2 -n 500 -m "full" -d "results/synthetic" -g "fast"
+./brem.r -f "data/synthetic.rdata" -k 1 -n 500 -m "full" -d "results/synthetic" -g "fast"
 
-seq 10|./parallel --sshlogin 8/d10 -j +0 'cd /extra/duboisc0/blockrem/; ./brem.r'
-seq 5|./parallel --sshlogin 8/d9,8/d10 'module load R/2.13.1; cd /extra/duboisc0/blockrem/; ./tmp.r'
-ls | parallel --sshlogin 8/d8,8/d9,8/d10 'echo -n {}" "; ls {}|wc -l'
+./brem.r -f "data/eckmann-small.rdata" -k 2 -n 500 -m "baserates" -d "results/eckmann-small" -g "fast"
+./brem.r -f "data/eckmann-small.rdata" -k 2 -n 500 -m "shared" -d "results/eckmann-small" -g "fast"
+./brem.r -f "data/eckmann-small.rdata" -k 2 -n 500 -m "full" -d "results/eckmann-small" -g "fast"
+./brem.r -f "data/eckmann-small.rdata" -k 1 -n 500 -m "full" -d "results/eckmann-small" -g "fast"
 
-./parallel --sshlogin 8/d8,8/d9,8/d10 'cd /extra/duboisc0/blockrem;./brem.r -f "data/synthetic.rdata" -k 2 -n 1000 -m {} -d "results/synthetic" -g "slow"'  ::: "full" "shared" "baserates"
+./parallel --sshlogin 8/d10,8/d11 'cd /extra/duboisc0/blockrem;./tmp.r {}' ::: 1 2 3
+./parallel --sshlogin 8/d10,8/d11 'cd /extra/duboisc0/blockrem;./brem.r -f \"data/eckmann-small.rdata\" -k 5 -n 500 -m {} -d \"results/eckmann-small\"' ::: "shared" "full"
+./parallel --sshlogin 8/d10,8/d'cd /extra/duboisc0/blockrem;./brem.r -f \"data/eckmann-small.rdata\" -k {} -n 500 -m {} -d \"results/eckmann-small\"' ::: 3 5 ::: "shared" "full"
+
+
+./dashboard.r -d "synthetic"
 
 cd ~/Documents/blockrem/
 rsync -auvz . duboisc@d1:/extra/duboisc0/blockrem/
-rsync -auvz duboisc@d1:/extra/duboisc0/blockrem/results blockrem/
+rsync -auvz duboisc@d1:/extra/duboisc0/blockrem/results .
+rsync -auvz duboisc@d1:/extra/duboisc0/blockrem/figs .
