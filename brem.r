6deb5de8b82bbcf985c63e92d4c4f0bb78f92236
chris <chris@doobwa.(none)>
2012-02-14 17:40:49 -0800

    Try and get errors on eckmann small post bug fix.

--- brem.r
+++ brem.r
@@ -24,10 +24,11 @@ library(brem)
 # Precompute data structures
 N <- max(c(A[,2],A[,3]))
 M <- nrow(A)
-P <- 12
+P <- 13
 s <- new(RemStat,A[,1],A[,2]-1,A[,3]-1,N,M,P)
 s$precompute()
 
 fit <- brem.mcmc(A,N,opts$numclusters,s,model.type=opts$model.type,
-		             niter=opts$numiterations,outdir=opts$outdir,gibbs=opts$gibbs)
+		 niter=opts$numiterations,outdir=opts$outdir,gibbs=opts$gibbs)
 
+fit <- brem.mcmc(A,N,5,s,model.type="full",niter=10,outdir="results/eckmann-small")
