bc98f1ff59b9ce7af5c50e11c9bdc0a97c77cd77
Chris DuBois <chris.dubois@gmail.com>
2012-02-19 19:32:45 -0800

    Pulled out prediction code so that I can run it with parallel.

---
+++ predict.r
@@ -0,0 +1,51 @@
+#!/usr/bin/env Rscript
+suppressPackageStartupMessages(library(optparse))
+suppressPackageStartupMessages(library(brem))
+
+option_list <- list(
+  make_option(c("-d", "--dataset"), 
+              help="Name of dataset with data at /data/[dataset].rdata 
+                    and results at /results/[dataset]/."),
+  make_option(c("-m", "--model"), default=NULL,
+              help="if not specified, baseline will be computed"),
+  make_option(c("-b", "--baseline"), default="uniform",
+              help="uniform, online, marg"))
+parser <- OptionParser(usage = "%prog [options]", option_list=option_list)
+opts   <- parse_args(OptionParser(option_list=option_list))
+
+options(verbose=FALSE)
+
+# Pull data from each saved file and grab the name of the fit
+load(paste("data/",opts$dataset,".rdata",sep=""))
+
+if (!is.null(opts$model)) {
+  f <- paste("results/",opts$dataset,"/",opts$model,".rdata",sep="")
+  load(f)
+  cat("precomputing\n")
+  P <- 13
+  strain <- new(RemStat,train[,1],as.integer(train[,2])-1,as.integer(train[,3])-1,N,nrow(train),P)
+  strain$precompute()
+  stest <- new(RemStat,test[,1],as.integer(test[,2])-1,as.integer(test[,3])-1,N,nrow(test),P)
+  stest$precompute()
+  cat("lambdas (train)\n")
+  lrm.train <- brem.lrm.fast(nrow(train), strain, res$z, res$beta)
+  cat("lambdas (test)\n")
+  lrm.test  <- brem.lrm.fast(nrow(test),   stest, res$z, res$beta)
+} else {
+  get.lrm <- switch(opts$baseline,
+                    "uniform" = function(x) array(1,c(nrow(x),N,N)),
+                    "online"  = function(x) ratemat.online(x,N),
+                    "marg" = function(x) ratemat.from.marginals(train,x,N))
+  cat("lambdas (train)\n")
+  lrm.train <- get.lrm(train)
+  cat("lambdas (test)\n")
+  lrm.test  <- get.lrm(test)
+}
+
+cat("ranks (train)\n")
+rk.train <- ranks(train,-lrm.train,ties.method="random")
+cat("ranks (test)\n")
+rk.test  <- ranks(test, -lrm.test, ties.method="random")
+
+dir.create(paste("results/",opts$dataset,"/ranks/",sep=""))
+save(rk.train,rk.test,opts,file=paste("results/",opts$dataset,"/ranks/",opts$model,".rdata",sep=""))
--- run.sh
+++ run.sh
@@ -27,14 +27,28 @@ cd /extra/duboisc0/blockrem;./brem.r -d "twitter-small" -k 1 -n 500 -m "full"
 # Run dashboard
 rsync -auvz dashboard.r duboisc@d1:/extra/duboisc0/blockrem/
 ./parallel --sshlogin 8/d10 'cd /extra/duboisc0/blockrem;./dashboard.r -d {} -s TRUE' ::: "synthetic" 
+./parallel --sshlogin 8/d10 'cd /extra/duboisc0/blockrem;./dashboard.r -d {} -s TRUE' ::: "twitter-small" 
 ./parallel --sshlogin 8/d6 'cd /extra/duboisc0/blockrem;./dashboard.r -d {} -s TRUE' ::: "synthetic" "eckmann-small" "twitter-small"
 
+./parallel --sshlogin 8/d10 'cd /extra/duboisc0/blockrem;./predict.r -d "synthetic" -m {}' ::: "full.1" "full.2" "shared.2"
+./parallel --sshlogin 8/d10 'cd /extra/duboisc0/blockrem;./predict.r -d "synthetic" -b {}' ::: "uniform" "online" "marg"
+
+./parallel --sshlogin 8/d10 'cd /extra/duboisc0/blockrem;./predict.r -d "eckmann-small" -m {}' ::: "full.1"
+./parallel --sshlogin 8/d10 'cd /extra/duboisc0/blockrem;./predict.r -d "eckmann-small" -b {}' ::: "uniform" "online" "marg"
+
+./parallel --sshlogin 8/d8 'cd /extra/duboisc0/blockrem;./predict.r -d "twitter-small" -m {}' ::: "full.1"
+./parallel --sshlogin 8/d7 'cd /extra/duboisc0/blockrem;./predict.r -d "twitter-small" -b {}' ::: "uniform" 
+
+cd /extra/duboisc0/blockrem;./predict.r -d "twitter-small" -b "uniform"
+cd /extra/duboisc0/blockrem;./predict.r -d "twitter-small" -b "online"
+cd /extra/duboisc0/blockrem;./predict.r -d "twitter-small" -b "marg"
 
 rsync -auvz . duboisc@d1:/extra/duboisc0/blockrem/
 
 cd ~/Documents/blockrem/
 rsync -auvz pkg duboisc@d1:/extra/duboisc0/blockrem/
 rsync -auvz brem.r duboisc@d1:/extra/duboisc0/blockrem/
+rsync -auvz predict.r duboisc@d1:/extra/duboisc0/blockrem/
 rsync -auvz data duboisc@d1:/extra/duboisc0/blockrem/
 rsync -auvz dashboard.r duboisc@d1:/extra/duboisc0/blockrem/
 rsync -auvz duboisc@d1:/extra/duboisc0/blockrem/results .
