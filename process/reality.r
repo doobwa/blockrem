7fd46b8e6a4aa37cbe88f7b3d9c2d7c6e479c2e5
Chris DuBois <chris.dubois@gmail.com>
2012-05-24 11:03:04 -0700

    Add Reality Mining dataset.

---
+++ process/reality.r
@@ -0,0 +1,34 @@
+x <- read.csv("data/realitymining/voice-all.edges",sep=";",header=FALSE)
+#unix_timestamp;caller_id;receiver_id;duration_in_second;communication_type
+colnames(x) <- c("time","sen","rec","dur","type")
+a <- read.csv('data/realitymining/all.attributes.txt',sep=";",header=FALSE)
+colnames(a) <- c("id","core")
+
+nodes <- a$id[which(a$core == 1)]
+A <- x[which(x$sen %in% nodes & x$rec %in% nodes),]
+
+A$sctime <- A$time - A$time[1]
+A$sctime <- A$sctime/max(A$sctime)
+A$sen <- A$sen + 1
+A$rec <- A$rec + 1
+A <- as.matrix(A[,c("sctime","sen","rec")])
+N <- length(nodes)
+test.ix <- 2001:2964
+train <- A[-test.ix,]
+test  <- A[test.ix,]
+save(A,N,train,test,test.ix,file="data/realitymining-sm.rdata")
+
+
+# Exploratory
+A$datetime <- as.POSIXlt(A$time, origin="1970-01-01", tz="America/New_York")
+dt <- strptime(A$datetime, "%Y-%m-%d %H:%M:%S", tz="UTC")
+datetimes <- as.data.frame(lapply(dt,unclass))
+datetimes$year <- 1900 + datetimes$year
+
+plot(datetimes$yday[1:2000],type="l")
+
+hist(datetimes$hour + datetimes$min/60,breaks=100)
+hist(datetimes$wday)
+
+md <- paste(datetimes$mon,datetimes$mday)
+tmp <- table(md)
