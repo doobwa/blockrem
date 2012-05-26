9ec3149eae1f5e325a6c2a00a7be9517d6af3087
Chris DuBois <chris.dubois@gmail.com>
2012-05-26 16:29:54 -0700

    Create brem(), a wrapper for mcmc.blockmodel()

--- pkg/R/brem.alt.r
+++ pkg/R/brem.alt.r
@@ -1,3 +1,19 @@
+brem <- function(train,N,K=2,effects=c("intercept","abba","abby","abay"),ego=TRUE,do.sm=FALSE,num.extra=2,niter=20,verbose=TRUE) {
+  M <- nrow(train)
+  P <- 13
+  ego <- ego*1  # RemStat doesn't want boolean
+  s <- new(RemStat,train[,1],as.integer(train[,2])-1,as.integer(train[,3])-1,N,M,P,ego)
+  s$precompute()
+  priors$px <- which(effects %in% c("intercept","abba","abby","abxa","abxb","abay","abab","sen_outdeg","rec_outdeg","sen_indeg","rec_indeg","dyad_count","changepoint_count"))
+  fit <- mcmc.blockmodel(lp,llk_node,priors,N,P,K,do.sm=do.sm,num.extra=num.extra,niter=niter,verbose=TRUE)
+
+  fit$ego <- ego
+  fit$beta <- fit$samples[[niter]]$phi
+  fit$z <- fit$samples[[niter]]$z
+  class(fit) <- "brem"
+  return(fit)
+}
+
 lp <- function(phi,z,priors) {
   K <- dim(phi)[2]
   P <- dim(phi)[1]
@@ -19,12 +35,12 @@ llk_node <- function(a,phi,z,priors) {
 # Requires s, train, and px to be in environment
 sample_phi <- function(phi,z,lpost,priors,kx=NULL) {
   K <- dim(phi)[2]
-  olp <- NULL
   if (is.null(kx)) kx <- 1:K
   for (k1 in kx) {
     for (k2 in kx) {
       cat(".")
-      for (p in px) {
+      olp <- NULL
+      for (p in priors$px) {
         brem.lpost.pk1k2 <- function(x,lp=TRUE) {
           k1nodes <- which(z==k1)
           k2nodes <- which(z==k2)
@@ -35,7 +51,7 @@ sample_phi <- function(phi,z,lpost,priors,kx=NULL) {
           return(pr.y + pr.phi)
         }
         res <- slice(phi[p,k1,k2],brem.lpost.pk1k2,m=20,olp=olp)
-#        olp <- attr(res,"log.density")
+        olp <- attr(res,"log.density")
         phi[p,k1,k2] <- res
       }
     }
--- synthetic.example.2.r
+++ synthetic.example.2.r
@@ -15,68 +15,35 @@ K <- opts$numclusters
 s <- new(RemStat,train[,1],as.integer(train[,2])-1,as.integer(train[,3])-1,N,M,P,ego)
 s$precompute()
 
-## Set priors
-priors <- list(alpha=1,sigma=.1,phi=list(mu=0,sigma=1))
-px <- rep(1,13)
-px[8:13] <- 0
-px[7] <- 0
-
-## Test basic likelihood functions
-K <- 2
-k1 <- k2 <- 1
-current <- array(rnorm(P*K^2,priors$phi$mu,priors$phi$sigma),c(P,K,K))
-olp <- brem.lpost.fast(A,N,K,z,s,beta)
-
-priors <- list(alpha=1,sigma=.1,phi=list(mu=0,sigma=1))
-llk <- sum(RemLogLikelihoodPc(beta,z-1,s$ptr(),K))
-llk <- sum(RemLogLikelihoodPcSubset(beta,z-1,s$ptr(),K,1:5))
-llk <- sum(RemLogLikelihoodActorPc(3,beta,z-1,s$ptr(),K))
-k1nodes <- which(z==k1)
-k2nodes <- which(z==k2)
-llk <- sum(RemLogLikelihoodBlockPc(k1-1,k2-1,k1nodes-1,k2nodes-1,beta,z-1,s$ptr(),K))
-
 # Fit model
+
 source("pkg/R/brem.alt.r")
-lp(beta,z,priors)
-px <- c(1,2,3,4,5,6)
-K <- 5; P <- 13
-fit <- mcmc.blockmodel(lp,llk_node,priors,N,P,K,do.sm=FALSE,num.extra=10,niter=15,verbose=TRUE)
-lp(fit$samples[[5]]$phi,fit$samples[[5]]$z,priors)
 
-fit <- mcmc.blockmodel(lp,llk_node,priors,N,P,10,do.sm=TRUE,num.extra=10,niter=20,verbose=TRUE)
-
-brem <- function(train,N,K=2,effects=c("intercept","abba","abby","abay"),ego=TRUE,do.sm=FALSE,num.extra=2,niter=20,verbose=TRUE) {
-  M <- nrow(train)
-  P <- 13
-  ego <- ego*1  # RemStat doesn't want boolean
-  s <- new(RemStat,train[,1],as.integer(train[,2])-1,as.integer(train[,3])-1,N,M,P,ego)
-  s$precompute()
-  px <- which(effects %in% c("intercept","abba","abby","abxa","abxb","abay","abab","sen_outdeg","rec_outdeg","sen_indeg","rec_indeg","dyad_count","changepoint_count"))
-  fit <- mcmc.blockmodel(lp,llk_node,priors,N,P,K,px=px,do.sm=do.sm,num.extra=num.extra,niter=niter,verbose=TRUE)
-
-  fit$ego <- ego
-  fit$beta <- fit$samples[[niter]]$phi
-  fit$z <- fit$samples[[niter]]$z
-  class(fit) <- "brem"
-  return(fit)
-}
 
 ## Set priors
 effects <- c("intercept","abba","abby","abay")
+
 priors <- list(alpha=1,sigma=.1,phi=list(mu=0,sigma=1))
 fit <- brem(train,N,K,effects)
 
 train.ix <- 1:nrow(train)
-p <- eval.online(A,N,train.ix,test.ix,fit,ties.method="random")
-q <- eval.online.baseline(A,N,train.ix,test.ix,"online",ties.method="random")
+p <- evaluate(A,N,train.ix,test.ix,fit,ties.method="random")
+q <- evaluate.baseline(A,N,train.ix,test.ix,"online",ties.method="random")
 
-
+par(mfrow=c(2,2))
+plot(p$llk$train,q$llk$train,xlab="model",ylab="baseline"); abline(0,1)
+plot(p$llk$test,q$llk$test,xlab="model",ylab="baseline"); abline(0,1)
+plot(p$mllk$train,q$mllk$train,xlab="model",ylab="baseline"); abline(0,1)
+plot(p$mllk$test,q$mllk$test,xlab="model",ylab="baseline"); abline(0,1)
 
 # Profile model fitting
 Rprof()
-fit <- mcmc.blockmodel(lp,llk_node,priors,N,P,K,px=c(1,2,3,4,5,6),do.sm=FALSE,num.extra=2,niter=20,verbose=TRUE)
+fit <- brem(train,N,K,effects)
 Rprof(NULL)
 summaryRprof()
+
+# Investigate role of prior
+
 
 #fit <- mcmc.blockmodel(lp,llk_node,priors,N,P,K,px=px,do.sm=opts$splitmerge,num.extra=opts$numextra,niter=as.numeric(opts$numiterations),verbose=TRUE,outfile=outfile)
 Rprof(NULL)
